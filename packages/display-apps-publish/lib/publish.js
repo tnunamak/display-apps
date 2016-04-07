5c788bec0d9384470eb49193b7132ff76482ebc9
Tim Nunamaker <tim.nunamaker@bazaarvoice.com>
2016-04-07 15:19:45 -0500

    Publish to S3

--- packages/display-apps-publish/lib/publish.js
+++ packages/display-apps-publish/lib/publish.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const aws = require('aws-sdk')
+
+const s3 = new aws.S3({ apiVersion: '2006-03-01' })
 
 module.exports = function (packageDir) {
   // TODO validate these things.
@@ -6,10 +9,29 @@ module.exports = function (packageDir) {
   const app = require(path.resolve(packageDir, manifest.main))
 
   const output = app.build()
+  const filename = `${manifest.name}-${manifest.version}.js`
 
-  const filename = `${manifest.name}-${manifest.version}.js:`
-  console.log(filename)
-  console.log(output)
+  const params = {
+    ACL: 'public-read',
+    Body: output,
+    // TODO: Get our own bucket.
+    Bucket: 'primary-ratings-summary',
+    ContentType: 'text/javascript;charset=UTF-8',
+    // Do not cache, for now.
+    CacheControl: 'max-age=0',
+    Key: `apps/${filename}`
+  }
 
-  // TODO upload output to S3
+  // TODO bail if file exists?
+  return new Promise(function (resolve, reject) {
+    s3.putObject(params, function(error, data) {
+      if (error) {
+        reject(`Uploading ${filename} failed: ${error}`)
+      }
+      else {
+        console.log(`Published to ${params.Bucket}/${params.Key} successfully.`)
+        resolve()
+      }
+    })
+  })
 }
