b4b7396d124d431507075942cdbe00a1cb8566fa
Tim Nunamaker <tim.nunamaker@bazaarvoice.com>
2016-04-08 17:57:01 -0500

    Client side code

--- packages/bv-say-hello/index.js
+++ packages/bv-say-hello/index.js
@@ -3,6 +3,6 @@ const random = require('bv-random-number')
 module.exports = {
   build: function () {
     n = Math.ceil(random.build())
-    return new Array(n).fill("hello").join(', ')
+    return 'console.log(' + new Array(n).fill("hello").join(', ') + ')'
   }
 }
