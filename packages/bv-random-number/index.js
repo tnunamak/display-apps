24f1bf7af5b7539c71a61bf829e4a15e256abf13
Tim Nunamaker <tim.nunamaker@bazaarvoice.com>
2016-04-08 18:35:50 -0500

    Consume config

--- packages/bv-random-number/index.js
+++ packages/bv-random-number/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   build: function (n) {
     n = n || 100
-    return 'console.log(' + (Math.random() * n).toFixed(2) + ')'
+    return 'console.log(' + (Math.random() * n).toFixed(2) + ')\n' +
+      'BV.bv-random-number.processConfig(function (cfg) { console.log("bv-random-number config", cfg) })'
   }
 }
